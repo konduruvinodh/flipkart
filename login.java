0b1143f8d5ed73ea8658a5b13aa7188f5633850a
konduruvinodh <vinodhkumar712@gmail.com>
2023-12-09 22:04:03 +0530

    number display

--- login.java
+++ login.java
@@ -1,6 +1,11 @@
-hi to class
-welcome all
-resnumagdajba
-daaviu	 xm
-ddaygmabj
-
+public class login
+{
+	public static void main(string) args[]
+		{
+			int i;
+			for(i=0;i<=10;i++)
+			{
+				system.out.println("Number: "+i);
+			}
+		}		
+}
